1be6524b2ac1233dba26d43529e17d2de85111f1
s1okouji <key_@outlook.jp>
2022-12-29 16:13:29 +0900

    Change dsn settings

--- service/database.go
+++ service/database.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"os"
+
 	"github.com/s1okouji/price_notify_api/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,7 +12,10 @@ var DB *gorm.DB
 
 func SetUp() {
 	var err error
-	dsn := "pna:pr1ce/Notify@tcp(127.0.0.1:3306)/price_notify?parseTime=true"
+	pass := os.Getenv("dbpass")
+	user := os.Getenv("dbuser")
+	host := os.Getenv("dbhost")
+	dsn := user + ":" + pass + "@tcp(" + host + ")/price_notify?parseTime=true"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
